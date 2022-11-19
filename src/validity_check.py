25461a4ae30c366cd615503da80a300a6d7bb594
Xinming Chen <chen_xin_ming@163.com>
2022-11-19 00:20:34 -0800

    Send emails in validity_check.py

--- src/validity_check.py
+++ src/validity_check.py
@@ -4,6 +4,7 @@ import csv
 import json
 import os
 import urllib.request
+import subprocess
 import time
 
 from bs4 import BeautifulSoup
@@ -49,14 +50,25 @@ class BaiduNetdisk(object):
             except Exception as e:
                 print("302：", e.__str__())
 
-game_db=[]
-with open('gamedb.json','r', encoding='utf-8-sig') as fp:
-  game_db=json.load(fp)
-for game_entry in game_db:
-  valid = BaiduNetdisk().analyze(game_entry['download_link'])
-  if valid:
-    print("OK      ",game_entry['title'])
-  else:
-    print("Invalid ",game_entry['title'])
-
+current_path = os.path.dirname(os.path.abspath(__file__))
+game_db = []
+with open(os.path.join(current_path,'gamedb.json'),'r', encoding='utf-8-sig') as fp:
+  game_db = json.load(fp)
+#Failed links will get retried up to 3 times.
+for retry in range(3):
+  invalid_games = []
+  for game_entry in game_db:
+    valid = BaiduNetdisk().analyze(game_entry['download_link'])
+    if not valid:
+      invalid_games.append(game_entry)
+  game_db = invalid_games
+if invalid_games:
+  email_subject = "pymo game download link broken"
+  email_body = "The following games become invalid:\n\n"
+  for game_entry in invalid_games:
+    email_body += game_entry['title']+"\n"
+  subprocess.call(["mailme.sh", email_subject, email_body])
+  print(email_body)
+else:
+  print("All pass.")
 
